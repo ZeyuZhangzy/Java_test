06f08162769152df90cca72356c7384adc4b1bd6
ZeyuZhangzy <zeyuzhang_zzy@outlook.com>
2021-09-13 13:56:08 +0800

    changed

--- src/main/java/com/company/threadLearn/thread.java
+++ src/main/java/com/company/threadLearn/thread.java
@@ -5,6 +5,7 @@ public class thread implements Runnable{
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello1");
+        //
     }
 
     public void run() {
